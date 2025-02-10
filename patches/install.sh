88a0b4772cc5c05398fa667d204a2ddd8a6dba05
Parthib24 <sirajammunirparthi24@gmail.com>
2025-02-10 22:16:20 +0100

    aine: Fix display issue for SPRD devices
    Change-Id: Id5f4dcb4a65b8ae41af5924b07e9205de3263143

---
+++ patches/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+C=$(pwd)
+S="energy/aine"
+D="bootable/recovery"
+
+apply_patches() { cd ${C}/${1}; git apply --ignore-whitespace ${C}/device/${S}/patches/$1/*.patch; cd ${C}; }
+
+for R in $D; do
+    apply_patches $R
+done
